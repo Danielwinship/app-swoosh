d1172f533449bf6957c8c66ace4fac4a3c21f40d
Daniel Winship <danielwinship@icloud.com>
2017-12-20 20:37:47 -0500

    built out model and wired up LeagueVC

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Daniel Winship on 12/20/17.
+//  Copyright © 2017 Daniel Winship. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLegue:String?
+    var selectedSkillLevel: String?
+    
+    
+}
