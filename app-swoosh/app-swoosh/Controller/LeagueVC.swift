5fad5fca9a66ae93dc6721de1ec834d15c62fb54
Daniel Winship <danielwinship@icloud.com>
2017-12-20 20:52:23 -0500

    built out segue to pass data to SkillVC

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
-    var player:Player!
+    //var player:Player!
+    var player = Player()
 
         @IBOutlet weak var nextButton:BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        player = Player()
+        //player = Player()
     }
 
 
@@ -43,15 +44,11 @@ class LeagueVC: UIViewController {
         nextButton.isEnabled = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let skillVC = segue.destination as? SkillVC {
+           skillVC.player = self.player
+        }
     }
-    */
+
 
 }
--- app-swoosh/app-swoosh/Controller/SkillVC.swift
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class SkillVC: UIViewController {
 
-    
+     var player = Player()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(player.desiredLegue)
         // Do any additional setup after loading the view.
     }
 
@@ -23,14 +23,5 @@ class SkillVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
